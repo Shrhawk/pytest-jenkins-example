2c5636b2a2542975e6b9b29c84e17ddb143a4e22
Syed Hasan raza <shrhawk88@gmail.com>
2023-06-07 19:58:32 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def print_variable():
     user_password = os.getenv("USER_PASSWORD")
     login_url = os.getenv("LOGIN_URL")
     success_url = os.getenv("SUCCESS_URL")
+    print(user_email, user_password, login_url, success_url)
 
 if __name__ == "__main__":
     print_variable()
